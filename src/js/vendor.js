d0b34314f57b27e65e35d09766c2a2f5d7492f11
Oleg Polyanskiy <ol.polyanskiy@gmail.com>
2021-02-18 22:49:37 +0300

    add smoothscroll polyfill

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,1 +1,5 @@
 import '../../node_modules/focus-visible/dist/focus-visible.min';
+import smoothscrollPolyfill from 'smoothscroll-polyfill';
+import 'smoothscroll-anchor-polyfill';
+
+smoothscrollPolyfill.polyfill();
