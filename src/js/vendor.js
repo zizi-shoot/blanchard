07aab856d222f8c615451337749474d3e6f404a3
Oleg Polyanskiy <ol.polyanskiy@gmail.com>
2021-02-23 17:41:55 +0300

    fix calc height of viewport

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -3,5 +3,13 @@ import smoothscrollPolyfill from 'smoothscroll-polyfill';
 import 'smoothscroll-anchor-polyfill';
 
 smoothscrollPolyfill.polyfill();
+
+/*
+ * Расчёт высоты вьюпорта для мобильных устройств
+ * Safari iOS по умолчанию не вычитает высоту адресной строки из 100vh
+ */
 let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+	vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
