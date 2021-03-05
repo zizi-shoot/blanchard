56befb08829392f85e680601b864bdd5de81983c
Oleg Polyanskiy <ol.polyanskiy@gmail.com>
2021-03-05 22:51:57 +0300

    fix calc vh

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -10,6 +10,7 @@ smoothscrollPolyfill.polyfill();
  * Safari iOS по умолчанию не вычитает высоту адресной строки из 100vh
  */
 let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 window.addEventListener('resize', () => {
 	vh = window.innerHeight * 0.01;
 	document.documentElement.style.setProperty('--vh', `${vh}px`);
