c3b0f9f3377c14026f589c555c95ac73228ee436
Oleg Polyanskiy <ol.polyanskiy@gmail.com>
2021-03-18 15:21:15 +0300

    fix font-family and update build

--- src/components/Catalog/catalog.js
+++ src/components/Catalog/catalog.js
@@ -7,132 +7,138 @@ const artistLists = document.querySelectorAll('.period-item__artist-list');
 const artistChoice = document.querySelector('.catalog-tab__choice');
 const tab = document.querySelector('.catalog__tab');
 const artistWrapper = document.querySelector('.catalog-tab__artist-wrapper');
+const picture = document.querySelector('.artist__picture');
 const artist = {
-    img: document.querySelector('.artist__pic'),
-    name: document.querySelector('.artist__name'),
-    lifeTime: document.querySelector('.artist__lifetime'),
-    descr: document.querySelector('.artist__descr'),
+  picture,
+  srcSet1025: picture.querySelector('[media="(min-width: 1025px)"]'),
+  srcSet577: picture.querySelector('[media="(min-width: 577px)"]'),
+  srcSet: picture.querySelector('source:nth-last-of-type(1)'),
+  name: document.querySelector('.artist__name'),
+  lifeTime: document.querySelector('.artist__lifetime'),
+  descr: document.querySelector('.artist__descr'),
 
-    set imgSrc(value) {
-        this.img.setAttribute('src', value);
-    },
-    set nameVal(value) {
-        this.name.innerText = value;
-    },
-    set lifeTimeVal(value) {
-        this.lifeTime.innerText = value;
-    },
-    set descrVal(value) {
-        this.descr.innerText = value;
-    },
+  set srcSets(value) {
+    this.srcSet1025.srcset = `img/${value}@2x.jpg 2x, img/${value}.jpg`;
+    this.srcSet577.srcset = `img/${value}@2x-tab.jpg 2x, img/${value}-tab.jpg`;
+    this.srcSet.srcset = `img/${value}-tab.jpg`;
+  },
+  set nameVal(value) {
+    this.name.innerText = value;
+  },
+  set lifeTimeVal(value) {
+    this.lifeTime.innerText = value;
+  },
+  set descrVal(value) {
+    this.descr.innerText = value;
+  },
 };
 const selectedArtist = {
-    fra: {
-        period: 0,
-        artistIndex: 11,
-    },
-    ger: {
-        period: 0,
-        artistIndex: 11,
-    },
-    ita: {
-        period: 0,
-        artistIndex: 11,
-    },
-    rus: {
-        period: 0,
-        artistIndex: 11,
-    },
-    bel: {
-        period: 0,
-        artistIndex: 11,
-    },
+  fra: {
+    period: 0,
+    artistIndex: 11,
+  },
+  ger: {
+    period: 0,
+    artistIndex: 11,
+  },
+  ita: {
+    period: 0,
+    artistIndex: 11,
+  },
+  rus: {
+    period: 0,
+    artistIndex: 11,
+  },
+  bel: {
+    period: 0,
+    artistIndex: 11,
+  },
 };
 const accordion = new Accordion('.accordion-container');
 
 function setCountryDescr(country) {
-    countryDescr.innerText = catalog.descrData[country];
+  countryDescr.innerText = catalog.descrData[country];
 }
 
 function setArtistList(country) {
-    artistLists.forEach((list) => {
-        const periodIndex = Array.from(artistLists).indexOf(list);
-        const artistNames = catalog.artistList[country][periodIndex];
-        for (const index in artistNames) {
-            const name = artistNames[index];
-            const listItem = `
+  artistLists.forEach((list) => {
+    const periodIndex = Array.from(artistLists).indexOf(list);
+    const artistNames = catalog.artistList[country][periodIndex];
+    for (const index in artistNames) {
+      const name = artistNames[index];
+      const listItem = `
 				<li class="period-item__artist-item">
 					<button class="period-item__btn-artist" data-artist-btn="${country}-${periodIndex}-${+index}">${name}</button>
 				</li>
 			`;
-            list.insertAdjacentHTML('beforeend', listItem);
-        }
-    });
+      list.insertAdjacentHTML('beforeend', listItem);
+    }
+  });
 }
 
 function setArtistBio(country, artistIndex) {
-    const {
-        lifeTime, descr, name, imgSrc,
-    } = catalog.artistBio[country][artistIndex];
+  const {
+    lifeTime, descr, name, picName,
+  } = catalog.artistBio[country][artistIndex];
 
-    artist.imgSrc = imgSrc;
-    artist.nameVal = name;
-    artist.lifeTimeVal = lifeTime;
-    artist.descrVal = descr;
+  artist.srcSets = picName;
+  artist.nameVal = name;
+  artist.lifeTimeVal = lifeTime;
+  artist.descrVal = descr;
 }
 
 function chooseArtist(ev) {
-    const [country, period, artistIndex] = ev.target.dataset.artistBtn.split('-');
-    artistWrapper.classList.add('catalog-tab__artist-wrapper--preanimated');
-    setTimeout(() => {
-        setArtistBio(country, artistIndex);
-        const activeBtn = artistChoice.querySelector('.period-item__btn-artist--active');
-        activeBtn.classList.remove('period-item__btn-artist--active');
-        ev.target.classList.add('period-item__btn-artist--active');
-        selectedArtist[country] = {
-            period: +period,
-            artistIndex: +artistIndex,
-        };
-        artistWrapper.classList.remove('catalog-tab__artist-wrapper--preanimated');
-    }, 300);
+  const [country, period, artistIndex] = ev.target.dataset.artistBtn.split('-');
+  artistWrapper.classList.add('catalog-tab__artist-wrapper--preanimated');
+  setTimeout(() => {
+    setArtistBio(country, artistIndex);
+    const activeBtn = artistChoice.querySelector('.period-item__btn-artist--active');
+    activeBtn.classList.remove('period-item__btn-artist--active');
+    ev.target.classList.add('period-item__btn-artist--active');
+    selectedArtist[country] = {
+      period: +period,
+      artistIndex: +artistIndex,
+    };
+    artistWrapper.classList.remove('catalog-tab__artist-wrapper--preanimated');
+  }, 300);
 }
 
 function setStartCatalog(country, period, artistIndex) {
-    setArtistList(country);
-    setCountryDescr(country);
-    setArtistBio(country, artistIndex);
+  setArtistList(country);
+  setCountryDescr(country);
+  setArtistBio(country, artistIndex);
 
-    const activeData = `${country}-${period}-${artistIndex}`;
-    const activeBtn = document.querySelector(`[data-artist-btn="${activeData}"]`);
+  const activeData = `${country}-${period}-${artistIndex}`;
+  const activeBtn = document.querySelector(`[data-artist-btn="${activeData}"]`);
 
-    activeBtn.classList.add('period-item__btn-artist--active');
-    accordion.open(+period);
+  activeBtn.classList.add('period-item__btn-artist--active');
+  accordion.open(+period);
 }
 
 function switchTab() {
-    tab.classList.add('catalog__tab--preanimated');
+  tab.classList.add('catalog__tab--preanimated');
 
-    setTimeout(() => {
-        accordion.closeAll();
-        countryBtns.forEach((btn) => btn.classList.remove('catalog-nav__btn--active'));
-        this.classList.add('catalog-nav__btn--active');
-        artistLists.forEach((list) => {
-            list.innerHTML = '';
-        });
-        const { country } = this.dataset;
-        const { artistIndex, period } = selectedArtist[country];
-        setStartCatalog(country, period, artistIndex);
-        accordion.open(+period);
-        tab.classList.remove('catalog__tab--preanimated');
-    }, 300);
+  setTimeout(() => {
+    accordion.closeAll();
+    countryBtns.forEach((btn) => btn.classList.remove('catalog-nav__btn--active'));
+    this.classList.add('catalog-nav__btn--active');
+    artistLists.forEach((list) => {
+      list.innerHTML = '';
+    });
+    const { country } = this.dataset;
+    const { artistIndex, period } = selectedArtist[country];
+    setStartCatalog(country, period, artistIndex);
+    accordion.open(+period);
+    tab.classList.remove('catalog__tab--preanimated');
+  }, 300);
 }
 
 setStartCatalog('ita', 0, 11);
 countryBtns.forEach((btn) => {
-    btn.addEventListener('click', switchTab);
+  btn.addEventListener('click', switchTab);
 });
 artistChoice.addEventListener('click', (ev) => {
-    if (ev.target.classList.contains('period-item__btn-artist')) {
-        chooseArtist(ev);
-    }
+  if (ev.target.classList.contains('period-item__btn-artist')) {
+    chooseArtist(ev);
+  }
 });
